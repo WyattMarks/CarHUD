5c56bd81a801ddba2c7cb2d89722c0b031122e64
Wyatt Marks <WyattMarks@users.noreply.github.com>
2022-05-22 01:36:31 -0500

    Get commands handled correctly, finally

--- CarHUD_PCB/CarHUD_PCB_Firmware/src/main.cpp
+++ CarHUD_PCB/CarHUD_PCB_Firmware/src/main.cpp
@@ -1,11 +1,43 @@
 #include <Arduino.h>
+char message[30];
+int messagePos;
+
+
+void handleCommand(char* message) {
+	if (strcmp(message, "name") == 0) {
+		Serial.print("ESP\n");
+	} else if (strcmp(message, "brightness") == 0) {
+		Serial.print(analogRead(33));
+		Serial.print('\n');
+	}
+}
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(33, INPUT);
+	Serial.begin(115200);
+	pinMode(33, INPUT);
+	messagePos = 0;
 }
 
 void loop() {
-  delay(1000);
-  Serial.println(analogRead(33));
+	uint x = Serial.available();
+	while (x > 0) {
+		char buffer[10];
+		x = Serial.readBytes(buffer, x);
+		
+		for (int i = 0; i < x; i++) {
+			message[messagePos + i] = buffer[i];
+		}
+		messagePos += x;
+
+		for (int i = messagePos - x; i < messagePos; i++) {
+			if (message[i] == '\n') {
+				message[i] = 0;
+				messagePos = 0;
+
+				handleCommand(message);
+			}
+		}
+
+		x = Serial.available();
+	}
 }
