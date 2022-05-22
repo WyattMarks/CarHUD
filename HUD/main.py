d1cb5d011e778ba35f25e252bc5b000da9dded5a
Wyatt Marks <inbox@wyattmarks.com>
2022-05-21 21:11:40 -0500

    Got brightness to display something meaningful

--- HUD/main.py
+++ HUD/main.py
@@ -1,5 +1,6 @@
 from sensors.gps import GPS
 from sensors.obd2 import OBD2
+from sensors.board import SensorBoard
 import pygame
 import os
 from time import sleep
@@ -14,11 +15,22 @@ class HUD():
         pygame.display.update()
         pygame.mouse.set_visible(False)
         self.gps = GPS()
-        self.obd = OBD2()
+        #self.obd = OBD2()
+        self.sensorBoard = SensorBoard()
 
-    def DrawText(self, text, font, center, color=pygame.Color(255,255,255)):
+    def GetTextWidth(self, text, font):
+        text_surface = font.render(text, True, color)
+        rect = text_surface.get_rect()
+        return rect.x
+
+    def DrawTextCentered(self, text, font, center, color=pygame.Color(255,255,255)):
         text_surface = font.render(text, True, color)
         rect = text_surface.get_rect(center=center)
+        self.lcd.blit(text_surface, rect)
+    
+    def DrawTextLeft(self, text, font, topleft, color=pygame.Color(255,255,255)):
+        text_surface = font.render(text, True, color)
+        rect = text_surface.get_rect(topleft=topleft)
         self.lcd.blit(text_surface, rect)
         
 
@@ -28,11 +40,19 @@ class HUD():
 
             coords = self.gps.GetCoords()
             if coords != None:
-                self.DrawText(f"Lat: {coords[0]:.5f}, Lon: {coords[1]:.5f}", self.largeFont, (1024/2, 600/3))
+                self.DrawTextCentered(f"Lat: {coords[0]:.5f}, Lon: {coords[1]:.5f}", self.largeFont, (1024/2, 600/3))
             
-            speed = self.obd.GetSpeed()
+            speed = 0#self.obd.GetSpeed()
             if speed != None:
-                self.DrawText(f"{speed}mph", self.largeFont, (1024/2, 500))
+                self.DrawTextCentered(f"{speed}mph", self.largeFont, (1024/2, 500))
+
+            brightness = self.sensorBoard.GetBrightness()
+            if brightness > 2500:
+                self.DrawTextLeft(f"BRIGHT!", self.largeFont, (0, 50))
+            elif brightness > 2000:
+                self.DrawTextLeft(f"Reasonable!", self.largeFont, (0, 50))
+            else:
+                self.DrawTextLeft(f"Dark!", self.largeFont, (0, 50))
 
             pygame.display.update()
             sleep(1)
